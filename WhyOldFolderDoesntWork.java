6dd30e9b4abba6eba9a7e313a6f7f6f7fc88b8b5
bobscomputer <katelynmcastro.klmc@gmail.com>
2025-09-27 16:49:23 -0700

    first commit of new repo for cs13 labs

---
+++ WhyOldFolderDoesntWork.java
@@ -0,0 +1,6 @@
+public class WhyOldFolderDoesntWork {
+    public static void main(String[] args) {
+        System.out.println("New folder for CS13 Projects because I botched the old one...\n" +
+                           "The main one's directory is all screwed up for VSCODE, but the code compiles and runs in JGRASP");
+    }
+}
